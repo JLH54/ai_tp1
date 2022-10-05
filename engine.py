fcf95bd439b17ed994a28cc951d3a7392b81c02f
JLH54 <julien_.com@hotmail.com>
2022-10-05 16:05:15 -0400

    added images

--- engine.py
+++ engine.py
@@ -1,11 +1,109 @@
 # This Python file uses the following encoding: utf-8
 import sys
 import pygame
+from PySide6.QtWidgets import QApplication
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.gameInit()
+
+    def loop(self):
+        self.timer.update()
+        dt = self.timer.get_deltaTime()
+
+        self.process_input()
+        self.gameLogic(dt)
+        self.render()
+
+    def process_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.should_quit = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.up_key_pressed = True
+                if event.key == pygame.K_s:
+                    self.down_key_pressed = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    self.up_key_pressed = False
+                if event.key == pygame.K_s:
+                    self.down_key_pressed = False
+
+
+    def gameInit(self):
+        self.size = self.width, self.height = 640, 480
+        self.black = 0,0,0
+
+        self.screen = pygame.display.set_mode(self.size)
+
+    def gameLogic(self, dt):
+        pass
+
+    def render(self):
+        self.screen.fill(self.black)
+
+        pygame.display.flip()
+
+class Window(QWidget):
+    started:bool
+    def __init__(self, game):
+        super().__init__()
+        self.started = False
+        self.game = game
+        self.initUi()
+        self.init_pygame(self.game)
+
+
+    def init_pygame(self, game):
+        self.game = game
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.pygame_loop)
+        self.timer.start(0)
+
+    def pygame_loop(self):
+        if self.game.loop():
+            self.close()
+
+    def initUi(self):
+        self.setWindowTitle("Py pong")
+        self.setGeometry(0,400,300,200)
+
+
+        self.button = QPushButton("Start Game", self)
+        self.button.move(100, 150)
+        self.button.clicked.connect(self.OnClick)
+
+        self.slider = QSlider(self)
+        self.slider.sliderReleased.connect(self.OnSlider)
+        self.slider.setRange(1, 3)
+        self.slider.setSingleStep(1)
+
+
+        self.show()
+
+    def OnClick(self):
+        self.started = True
+        pass
+
+    def returnStart(self):
+        return self.started
+
+    def returnDifficulty(self):
+        return self.slider.TickPosition
+
+    def OnSlider(self):
+        slider:QSlider = self.sender()
+        self.game.changeDifficulty(slider.value())
+        print(slider.value())
+        pass
 
 class engine:
     id = [[]]
     def __init__(self):
-
+        game = Game()
+        exe = Window(game)
         pass
 
     def initEngine(self):
@@ -22,4 +120,6 @@ class engine:
         self.greenTile = 50, 50 ,0
         self.setWindowTitle("Checkers")
         self.setGeometry(0,400,300,200)
+
+
 
--- main.py
+++ main.py
@@ -1,18 +1,168 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtWidgets import QApplication
 
-import engine
+import pygame
+import random
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QSlider,QLabel
+from PySide6.QtCore import QTimer
+
+class Timer:
+    _clock = None
+    _dt:float = 0.016
+
+    def __init__(self):
+        self._clock = pygame.time.Clock()
+
+    def update(self):
+        self._dt = self._clock.tick(60)/1000
+
+    def get_deltaTime(self):
+        return self._dt
+
+class Game:
+    white = (255,255,255)
+    black = (0, 0 ,0)
+    board = (171 , 79,9)
+    grid = []
+    width:int
+    height:int
+    margin = 5
+    def __init__(self):
+        pygame.init()
+        self.gameInit()
+        self.timer = Timer()
+        self.width= 1000 / 8
+        self.height = 720 / 8
+        self.playerImageSrc = "images/RedPawn.png"
+        self.aiImageSrc = "images/GreyPawn.png"
+        self.should_quit = False
+        self.up_key_pressed = False
+        self.down_key_pressed = False
+
+    def changeDifficulty(self, Gamedifficulty):
+        self.difficulty = Gamedifficulty
+
+    def loop(self):
+        self.timer.update()
+        dt = self.timer.get_deltaTime()
+
+        self.process_input()
+        self.gameLogic(dt)
+        self.render()
+
+    def process_input(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.should_quit = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.up_key_pressed = True
+                if event.key == pygame.K_s:
+                    self.down_key_pressed = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_w:
+                    self.up_key_pressed = False
+                if event.key == pygame.K_s:
+                    self.down_key_pressed = False
+
+    def gameInit(self):
+        self.size = self.width, self.height = 1000, 720
+        self.src = pygame.display.set_mode([700,520])
+        for row in range(8):
+            self.grid.append([])
+            for column in range(8):
+                self.grid[row].append(0)
+        self.started = False
+
+        self.screen = pygame.display.set_mode(self.size)
+
+    def gameLogic(self, dt):
+        #player behaviour
+        #ai behaviour
+        pass
+
+    def render(self):
+        self.screen.fill(self.board)
+        for row in range(8):
+            for column in range(8):
+                if(row % 2 == 0):
+                    if(column % 2 == 1):
+                        self.color = self.white
+                    else:
+                        self.color = self.black
+                if(row % 2 == 1):
+                    if(column % 2 == 0):
+                        self.color = self.white
+                    else:
+                        self.color = self.black
+                pygame.draw.rect(self.src, self.color, [self.width * column, self.height * row , self.width, self.height])
+        pygame.display.flip()
+
+class Window(QWidget):
+    started:bool
+    def __init__(self, game):
+        super().__init__()
+        self.started = False
+        self.game = game
+        self.initUi()
+        self.init_pygame(self.game)
+
+
+    def init_pygame(self, game):
+        self.game = game
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.pygame_loop)
+        self.timer.start(0)
+
+    def pygame_loop(self):
+        if self.game.loop():
+            self.close()
+
+    def initUi(self):
+        self.setWindowTitle("Py pong")
+        self.setGeometry(0,400,300,200)
+
+
+        self.button = QPushButton("Start Game", self)
+        self.button.move(100, 150)
+        self.button.clicked.connect(self.OnClick)
+
+        #settings for ai
+        self.label1 = QLabel("AI difficulty", self)
+        self.label1.move(80, 85)
+
+        self.aislider = QSlider(self)
+        self.aislider.sliderReleased.connect(self.OnSlider)
+        self.aislider.setRange(1, 3)
+        self.aislider.setSingleStep(1)
+        self.aislider.move(100, 1)
+
+        self.show()
+
+    def OnClick(self):
+        self.started = True
+        pass
+
+    def returnStart(self):
+        return self.started
+
+    def returnDifficulty(self):
+        return self.slider.TickPosition
+
+    def OnSlider(self):
+        slider:QSlider = self.sender()
+        #self.game.changeDifficulty(slider.value())
+        print(slider.value())
+        pass
 
 def main():
     app = QApplication(sys.argv)
+    game = Game()
+    exe = Window(game)
 
-    Engine = engine()
-
-    Engine.initScreen()
-
+    app.setActiveWindow(exe)
+    # ...
     sys.exit(app.exec())
-
 
 if __name__ == "__main__":
     main()
